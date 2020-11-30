855da5a309d3b5a79b3a290e2c7c30dc9d05f1f2
thepaperpansy <kristinmabram@gmail.com>
2020-11-30 17:31:37 -0600

    add variables

--- dice_roller.py
+++ dice_roller.py
@@ -1,14 +1,15 @@
 import random
 
 def main():
-  dice_rolls = 2
+  dice_rolls = int(input('How many dice would you like to roll?'))
+  dice_sides = int(input('How many sides are on the dice?'))
   dice_sum = 0
   for i in range(0, dice_rolls):
-    roll = random.randint(1,6)
+    roll = random.randint(1,dice_sides)
     dice_sum += roll
     if roll ==1:
       print(f'You rolled a {roll}! Critical Fail')
-    elif roll ==6:
+    elif roll ==dice_sides:
       print(f'You rolled a {roll}! Critical Success')
     else:
       print(f'You rolled a {roll}')
